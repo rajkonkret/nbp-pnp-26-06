0227341fbe4d95371a243af5f5cdda7d4c58af1b
rajkonkret <rajkonkret660@gmail.com>
2023-06-29 13:41:03 +0200

    add wyjres3

---
+++ wykres3.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+labels = ['Jabłka', 'Banany', 'Winogrona', 'Pomarańcze']
+sizes = [30, 25, 20, 45]
+colors = ['red', 'blue', 'purple', 'yellow']
+
+plt.pie(sizes, labels=labels, colors=colors,
+        autopct='%1.1f%%')
+plt.title("Wykres kołowy")
+plt.axis('equal')
+
+plt.show()
